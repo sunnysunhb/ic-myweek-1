84a4a82fe362e7f1aed806d66f290ec7daeffd8e
unknown <sunny_sun_hb@outlook.com>
2025-02-23 16:12:15 +1300

    change triangle.cs

--- Classes/Triangle.cs
+++ Classes/Triangle.cs
@@ -9,9 +9,32 @@ namespace Week1ObjectOriented.Classes
 {
     public class Triangle : Shape, IFaces, IShape
     {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle(double a, double b, double c)
+        {
+
+            if (a > 10 || b > 10 || c > 10)
+            {
+                throw new Exception("Cannot be greater then 10");
+            }
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                throw new Exception("Please recheck the sides");
+            }
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
         public double GetArea()
         {
-            throw new NotImplementedException();
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c)); ;
         }
 
         public int GetFaces()
